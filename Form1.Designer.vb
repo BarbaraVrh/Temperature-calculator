7b80154af6a5f0284bffbcf4062de85fbe158694
Barbara Vrhovac <89888012+BarbaraVrh@users.noreply.github.com>
2024-01-11 17:16:22 -0600

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblTempF = New Label()
+        txtTempF = New TextBox()
+        btnConvert = New Button()
+        lblTempC = New Label()
+        txtTempC = New TextBox()
+        SuspendLayout()
+        ' 
+        ' lblTempF
+        ' 
+        lblTempF.AutoSize = True
+        lblTempF.Location = New Point(117, 51)
+        lblTempF.Name = "lblTempF"
+        lblTempF.Size = New Size(174, 20)
+        lblTempF.TabIndex = 0
+        lblTempF.Text = "Temperature(Fahrenheit):"
+        ' 
+        ' txtTempF
+        ' 
+        txtTempF.Location = New Point(331, 48)
+        txtTempF.Name = "txtTempF"
+        txtTempF.Size = New Size(119, 27)
+        txtTempF.TabIndex = 1
+        ' 
+        ' btnConvert
+        ' 
+        btnConvert.Location = New Point(229, 103)
+        btnConvert.Name = "btnConvert"
+        btnConvert.Size = New Size(171, 31)
+        btnConvert.TabIndex = 2
+        btnConvert.Text = "Convert to Celsius"
+        btnConvert.UseVisualStyleBackColor = True
+        ' 
+        ' lblTempC
+        ' 
+        lblTempC.AutoSize = True
+        lblTempC.Location = New Point(117, 177)
+        lblTempC.Name = "lblTempC"
+        lblTempC.Size = New Size(151, 20)
+        lblTempC.TabIndex = 3
+        lblTempC.Text = "Temperature(Celsius):"
+        ' 
+        ' txtTempC
+        ' 
+        txtTempC.Location = New Point(331, 174)
+        txtTempC.Name = "txtTempC"
+        txtTempC.ReadOnly = True
+        txtTempC.Size = New Size(119, 27)
+        txtTempC.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(559, 219)
+        Controls.Add(txtTempC)
+        Controls.Add(lblTempC)
+        Controls.Add(btnConvert)
+        Controls.Add(txtTempF)
+        Controls.Add(lblTempF)
+        Name = "Form1"
+        Text = "Temperature"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblTempF As Label
+    Friend WithEvents txtTempF As TextBox
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents lblTempC As Label
+    Friend WithEvents txtTempC As TextBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Dim fahrenheitTemp, celsiusTemp As Double
+        fahrenheitTemp = CDbl(txtTempF.Text)
+        celsiusTemp = FtoC(fahrenheitTemp)
+        txtTempC.Text = CStr(celsiusTemp)
+
+    End Sub
+    Function FtoC(t As Double) As Double
+        Return Math.Round((5 / 9) * (t - 32), 2)
+    End Function
+
+    Private Sub frmConvert_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub txtTempF_TextChanged(sender As Object, e As EventArgs) Handles txtTempF.TextChanged
+
+    End Sub
+End Class
